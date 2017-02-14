60e6e8b5977b89a4a83ca45f0fba30460488080e
peayad <peayad@gmail.com>
2017-02-14 16:51:58 +0200

    Initial commit

---
+++ app/src/main/java/iti_edu/battuta/PickLocationActivity.java
@@ -0,0 +1,13 @@
+package iti_edu.battuta;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PickLocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pick_location);
+    }
+}
