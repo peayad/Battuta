bffdd0c31f86afc2e1918603eafb2f91088131c1
peayad <peayad@gmail.com>
2017-02-18 10:16:30 +0200

    Feedback menuItem sends an email to battuta@iti.com Google Places API now works!! Google Navigation works too!! Haba2 agarab fel DB .. msh metamen :/

--- app/src/main/java/iti_edu/battuta/EditTripActivity.java
+++ app/src/main/java/iti_edu/battuta/EditTripActivity.java
@@ -1,15 +1,22 @@
 package iti_edu.battuta;
 
-import android.content.Context;
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.content.Intent;
+import android.icu.text.SimpleDateFormat;
+import android.icu.util.Calendar;
 import android.net.Uri;
+import android.os.Build;
 import android.support.annotation.NonNull;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
@@ -20,7 +27,8 @@ import com.google.android.gms.location.places.Places;
 import com.google.android.gms.location.places.ui.PlaceSelectionListener;
 import com.google.android.gms.location.places.ui.SupportPlaceAutocompleteFragment;
 
-import java.net.URI;
+import java.util.Date;
+
 
 public class EditTripActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener {
 
@@ -30,6 +38,9 @@ public class EditTripActivity extends AppCompatActivity implements GoogleApiClie
     private GoogleApiClient mGoogleApiClient;
 
     private Place startPlace, endPlace;
+    private EditText dateTimeET;
+
+    private int tripYear, tripMonth, tripDay, tripHour, tripMinute;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -67,14 +78,63 @@ public class EditTripActivity extends AppCompatActivity implements GoogleApiClie
             }
         });
 
+
+        dateTimeET = (EditText) findViewById(R.id.edit_date_time);
+        dateTimeET.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onClick(View v) {
+                startDateDialog();
+            }
+        });
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    void startTimeDialog(){
+        TimePickerDialog timePicker = new TimePickerDialog(EditTripActivity.this, new TimePickerDialog.OnTimeSetListener() {
+            @RequiresApi(api = Build.VERSION_CODES.N)
+            @Override
+            public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                tripHour = selectedHour;
+                tripMinute = selectedMinute;
+
+                Date selectedDate = new Date(tripYear, tripMonth, tripDay, tripHour, tripMinute);
+                SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/YY hh:mm a");
+                String dateString = dateFormat.format(selectedDate);
+
+                dateTimeET.setText(dateString);
+
+                Log.i(TAG, dateString);
+            }
+        }, Calendar.HOUR, Calendar.MINUTE, false);
+        timePicker.setTitle("Select Time");
+        timePicker.show();
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    void startDateDialog(){
+        DatePickerDialog datePickerDialog = new DatePickerDialog(EditTripActivity.this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker view, int selectedYear, int selectedMonth, int selectedDay) {
+                tripYear = selectedYear;
+                tripMonth = selectedMonth;
+                tripDay = selectedDay;
+                startTimeDialog();
+            }
+        }, 2017,Calendar.MONTH,Calendar.DAY_OF_MONTH );
+        datePickerDialog.setTitle("Select Date");
+        datePickerDialog.show();
     }
 
     Uri getDirectionsURI(Place sPlace, Place ePlace) {
-        // http://maps.google.com/maps?saddr= lat,long &daddr= lat,long
+        /*
+            the map http link format should be like this:
+            http://maps.google.com/maps?saddr= lat,long &daddr= lat,long
+        */
         String startLatLng = startPlace.getLatLng().latitude + "," + startPlace.getLatLng().longitude;
         String endLatLng = endPlace.getLatLng().latitude + "," + endPlace.getLatLng().longitude;
-        Uri directionURI = Uri.parse("http://maps.google.com/maps?saddr=" + startLatLng + "&daddr=" + endLatLng);
-        return directionURI;
+        return Uri.parse("http://maps.google.com/maps?saddr=" + startLatLng + "&daddr=" + endLatLng);
     }
 
     @Override
